a32387fb5ab8544bf27d7cfe461c2c222d78bdcc
mfischer9 <36900387+mfischer9@users.noreply.github.com>
2018-05-03 00:25:01 -0500

    Added row and col int, from GridPane location

--- BracketNode.java
+++ BracketNode.java
@@ -3,8 +3,28 @@ package application;
 public class BracketNode {
 	Challenger cOne; 
 	Challenger cTwo;
+	//boolean created so the next round is generated
+	//once all scores have been submitted in a round
+	boolean scoreSubmitted;
+	//The number of the game starting at game 0
+	//Used to determine where the winner goes in the next round.
+	Integer gameNumber;
+	int row;
+	int col;
 	
-	public BracketNode() {
+	public void setRowAndCol(int col, int row) {
+		this.col = col;
+		this.row = row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public int getRow() {
+		return row;
+	}
+	
+	public BracketNode(int gameNumber) {
+		this.gameNumber = gameNumber;
 		cOne = null;
 		cTwo = null;
 	}
@@ -23,16 +43,35 @@ public class BracketNode {
 		return cTwo;
 	}
 	
+	public void setScoreSubmitted(boolean value) {
+		this.scoreSubmitted = value;
+	}
+	
 	public Challenger getWinner() {
 		if (cOne.getCurrScore() > cTwo.getCurrScore()) {
 			return cOne;
 		}
 		return cTwo;
 	}
+	
+	public boolean getScoreSubmitted() {
+		return this.scoreSubmitted;
+	}
+	
+	public Integer getGameNumber() {
+		return this.gameNumber;
+	}
+	
 	public Challenger getLoser() {
 		if (cOne.getCurrScore() < cTwo.getCurrScore()) {
 			return cOne;
 		}
 		return cTwo;
+	}
+	public boolean hasTie() {
+		if (cOne.getCurrScore() == cTwo.getCurrScore()) {
+			return true;
+		}
+		return false;
 	}
 }

133cfa2974f9fbf078e769ba03e00ac8b064d285
mfischer9 <36900387+mfischer9@users.noreply.github.com>
2018-05-03 00:24:13 -0500

    Added additional functionality
    including getThirdPlace()

--- Bracket.java
+++ Bracket.java
@@ -5,76 +5,92 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bracket {
-	BracketNode[] challenges;
-	int numChallenges;
-	int numChallengers;
-	
-	public Bracket(String filename) throws IOException{
-		
-		// Read in file
-		File file = new File(filename);
-		Scanner sc = new Scanner(file);
-		
-		ArrayList<String> teams = new ArrayList<String>();
-		while(sc.hasNextLine()) {
-			teams.add(sc.nextLine());
-		}
-		numChallengers = teams.size();
-		numChallenges = numChallengers - 1;
-		challenges = new BracketNode[numChallenges];
-		for (int i = 0; i < numChallenges; i++) {
-			challenges[i] = new BracketNode();
-		}
-		
-		for (int j = 0; j <= numChallenges/2; j++) {
-			challenges[j].setCOne(new Challenger(j, teams.get(j)));
-		}
-		for (int k = numChallenges/2; k >= 0; k--) {
-			challenges[k].setCTwo(new Challenger(k, teams.get(numChallenges - k)));
-		}
-		
-		sc.close();
-	}
-	
-	public int getNumChallenges() {
-		return numChallenges;
-	}
-	
-	public BracketNode[] getChallenges() {
-		return challenges;
-	}
-	
-	public BracketNode[] updateChallenge(int cNum) {
-		if (cNum % 2 == 0) {
-			challenges[cNum + (numChallengers-cNum)/2].setCOne(challenges[cNum].getWinner());
-		}
-		else {
-			challenges[cNum + (numChallengers-cNum)/2].setCTwo(challenges[cNum].getWinner());
-		}
-		return challenges;
-	}
-	
-	public String printBracket() {
-		String output = "";
-		for (int i = 0; i < numChallenges; i++) {
-			if (i == numChallenges - 1) {
-				output += ("\n" + "CHAMPIONSHIP\n");
-			}
-			else if (i == numChallenges - 3) { 
-				output += ("\n" + "SEMI-FINAL\n");
-			}
-			else if (i == numChallenges - 7) {
-				output += ("\n" + "QUARTER-FINAL\n");
-			}
-			else if (i == numChallenges - 15) {
-				output += ("\n" + "1st ROUND\n");
-			}
-			output += "game" + (i+1) + ": " + challenges[i].getCOne().getName() 
-					              + " vs. " + challenges[i].getCTwo().getName() + "\n";
-		}
-		return output;
-	}
-	
-}
+    BracketNode[] challenges;
+    int nChallenges;
+    int nChallengers;
     
-	
+    public Bracket(String filename) throws IOException{
+        
+        // Read in file
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        
+        ArrayList<String> teams = new ArrayList<String>();
+        while(sc.hasNextLine()) {
+            teams.add(sc.nextLine());
+        }
+        nChallengers = teams.size();
+        nChallenges = nChallengers - 1;
+        challenges = new BracketNode[nChallenges];
+        for (int i = 0; i < nChallenges; i++) {
+            challenges[i] = new BracketNode(i);
+        }
+        
+        for (int j = 0; j <= nChallenges/2; j++) {
+            challenges[j].setCOne(new Challenger(teams.get(j)));
+        }
+        for (int k = nChallenges/2; k >= 0; k--) {
+            challenges[k].setCTwo(new Challenger(teams.get(nChallenges - k)));
+        }
+        
+        sc.close();
+    }
+    
+    public int getNumChallenges() {
+        return nChallenges;
+    }
+    
+    public int getNumChallengers() {
+    		return nChallengers;
+    }
+    
+    public BracketNode[] getChallenges() {
+    		return challenges;
+    }
+    
+    public BracketNode getChallenge(int cNum) {
+        return challenges[cNum];
+    }
+    
+    public BracketNode updateChallenge(int cNum) {
+        if (cNum % 2 == 0) {
+            challenges[cNum + (nChallengers-cNum)/2].setCOne(challenges[cNum].getWinner());
+        }
+        else {
+            challenges[cNum + (nChallengers-cNum)/2].setCTwo(challenges[cNum].getWinner());
+        }
+        return challenges[cNum + (nChallengers-cNum)/2];
+    }
+    
+   
+    public Challenger getThirdPlace() {
+        Challenger cOne = challenges[nChallenges -1].getLoser();
+        Challenger cTwo = challenges[nChallenges-2].getLoser();
+        if (cOne.getCurrScore() > cTwo.getCurrScore()) {
+        		return cOne;
+        }
+        return cTwo;
+    }
+    
+    public String printBracket() {
+        String output = "";
+        for (int i = 0; i < nChallenges; i++) {
+            if (i == nChallenges - 1) {
+                output += ("\n" + "CHAMPIONSHIP\n");
+            }
+            else if (i == nChallenges - 3) { 
+                output += ("\n" + "SEMI-FINAL\n");
+            }
+            else if (i == nChallenges - 7) {
+                output += ("\n" + "QUARTER-FINAL\n");
+            }
+            else if (i == nChallenges - 15) {
+                output += ("\n" + "1st ROUND\n");
+            }
+            output += "game" + (i+1) + ": " + challenges[i].getCOne().getName() 
+                                  + " vs. " + challenges[i].getCTwo().getName() + "\n";
+        }
+        return output;
+    }
+    
+}

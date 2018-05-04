2b723deed49a299f5153a2c2a10f7bf1a1721b0c
mfischer9 <36900387+mfischer9@users.noreply.github.com>
2018-05-03 20:59:56 -0500

    Commented

--- Challenger.java
+++ Challenger.java
@@ -1,28 +1,43 @@
 package application;
 
-
+/**
+ * This class represents a competitor with a name and score.
+ */
 public class Challenger {
+	// The score for the challenger within their current competition
 	private int currScore; 
+	// The challenger's name
 	private String name;
+	// A string version of score, to be used with a GUI
 	private String currScoreString;
 	
+	/** Constructor initializes the challenger's name */
 	public Challenger(String name) {
 		this.name = name;
 	}
+	
+	/** Return the challenger's current score (int) */
 	public int getCurrScore() {
 		return currScore;
 	}
+	
+	/** Return the challenger's current score (String) */
 	public String getCurrScoreString() {
 		return currScoreString;
 	}
+	
+	/** Update the challenger's current score (int) */
 	public void setCurrScore(int score) {
 		currScore = score;
 	}
+	
+	/** Update the challenger's current score (String) */
 	public void setCurrScoreString(String score) {
 		currScoreString = score;
 	}
+	
+	/** Return the challenger's name */
 	public String getName() {
 		return name;
 	}
-	
 }
